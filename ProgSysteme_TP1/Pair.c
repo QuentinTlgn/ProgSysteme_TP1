2a94a965f285065292dba42d393e8c439e961a3e
Quentin TAULEIGNE <tauleigq@3il.fr>
2024-05-30 15:47:41 +0200

    uyk

---
+++ ProgSysteme_TP1/Pair.c
@@ -0,0 +1,13 @@
+/******************************************************
+Name    : Main.C
+Role    : Composant du TP1 1du cours de Programmation Système
+Author  : TAULEIGNE Quentin
+Org.    : 3iL Ingénieurs Rodez promotion 22
+Date    : 30/05/2024
+*******************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+}
