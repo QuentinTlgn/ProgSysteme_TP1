40da5f0b2488f46b3d2dc763ab551554df5f3d99
Quentin TAULEIGNE <tauleigq@3il.fr>
2024-06-05 10:57:13 +0200

    Implémentation de l'alternance

--- Main/Main/Main.c
+++ Main/Main/Main.c
@@ -1,9 +1,9 @@
 /******************************************************
 Name    : Main.C
-Role    : Main du TP1 1du cours de Programmation Système
+Role    : Main du TP1 du cours de Programmation Systeme
 Author  : TAULEIGNE Quentin
-Org.    : 3iL Ingénieurs Rodez promotion 22
-Date    : 30/05/2024
+Org.    : 3iL Ingenieurs Rodez promotion 22
+Date    : 05/06/2024
 *******************************************************/
 
 
@@ -11,35 +11,63 @@ Date    : 30/05/2024
 #include <windows.h>
 
 #define multithread
+#define alternance
 
 #ifdef multithread
 int nombre = 500;
 
+#ifdef alternance
+volatile int tour = 0;
+#endif // alternance
+
+
 DWORD WINAPI pairThread(LPVOID lpParam) {
+#ifdef alternance
+    int p; p = 1;
+#endif // alternance
+
     for (int i = 2; i <= nombre; i += 2) {
+#ifdef alternance
+        while (tour != p){};
+#endif // alternance
+
         printf("Pair: %d\n", i);
+
+#ifdef alternance
+        tour = 0;
+#endif // alternance
     }
     return 0;
 }
 
 DWORD WINAPI impairThread(LPVOID lpParam) {
+#ifdef alternance
+    int p; p = 0;
+#endif // alternance
+
     for (int i = 1; i <= nombre; i += 2) {
+#ifdef alternance
+        while (tour != p){};
+#endif // alternance
         printf("Impair: %d\n", i);
+#ifdef alternance
+        tour = 1;
+#endif // alternance
     }
     return 0;
 }
-#endif
+#endif // multithread
 
 int main() {
 #ifdef multiprocess
 #include <windows.h>
 #include <tchar.h>
 
-    // Noms des programmes à exécuter
+    // Noms des programmes à executer
     LPCSTR applicationName1 = "U:\\C\\ProgSysteme_TP1\\Impair\\x64\\Release\\Impair.exe";
     LPCSTR applicationName2 = "U:\\C\\ProgSysteme_TP1\\Pair\\x64\\Release\\Pair.exe";
 
-    // Structures pour spécifier les informations de démarrage des processus
+    // Structures pour specifier les informations de demarrage des processus
     STARTUPINFOA startupInfo1, startupInfo2;
     PROCESS_INFORMATION processInfo1, processInfo2;
 
@@ -48,43 +76,43 @@ int main() {
     startupInfo1.cb = sizeof(startupInfo1);
     ZeroMemory(&processInfo1, sizeof(processInfo1));
 
-    // Initialiser les structures pour le deuxième processus
+    // Initialiser les structures pour le deuxieme processus
     ZeroMemory(&startupInfo2, sizeof(startupInfo2));
     startupInfo2.cb = sizeof(startupInfo2);
     ZeroMemory(&processInfo2, sizeof(processInfo2));
 
-    // Créer le premier processus
+    // Creer le premier processus
     if (!CreateProcessA(
         applicationName1,   // Nom de l'application
         NULL,               // Ligne de commande
-        NULL,               // Attributs de sécurité du processus
-        NULL,               // Attributs de sécurité du thread
-        FALSE,              // Héritage des handles
-        0,                  // Flags de création
+        NULL,               // Attributs de securite du processus
+        NULL,               // Attributs de securite du thread
+        FALSE,              // Heritage des handles
+        0,                  // Flags de creation
         NULL,               // Environnement
-        NULL,               // Répertoire courant
+        NULL,               // Repertoire courant
         &startupInfo1,      // Structure STARTUPINFO
         &processInfo1       // Structure PROCESS_INFORMATION
     )) {
-        // Si CreateProcess échoue, afficher un message d'erreur
+        // Si CreateProcess echoue, afficher un message d'erreur
         printf("CreateProcess1 failed (%d).\n", GetLastError());
         return 1;
     }
 
-    // Créer le deuxième processus
+    // Creer le deuxieme processus
     if (!CreateProcessA(
         applicationName2,   // Nom de l'application
         NULL,               // Ligne de commande
-        NULL,               // Attributs de sécurité du processus
-        NULL,               // Attributs de sécurité du thread
-        FALSE,              // Héritage des handles
-        0,                  // Flags de création
+        NULL,               // Attributs de securite du processus
+        NULL,               // Attributs de securite du thread
+        FALSE,              // Heritage des handles
+        0,                  // Flags de creation
         NULL,               // Environnement
-        NULL,               // Répertoire courant
+        NULL,               // Repertoire courant
         &startupInfo2,      // Structure STARTUPINFO
         &processInfo2       // Structure PROCESS_INFORMATION
     )) {
-        // Si CreateProcess échoue, afficher un message d'erreur
+        // Si CreateProcess echoue, afficher un message d'erreur
         printf("CreateProcess2 failed (%d).\n", GetLastError());
         return 1;
     }
@@ -95,7 +123,7 @@ int main() {
     // Attendre la fin des deux processus
     DWORD waitResult = WaitForMultipleObjects(2, processHandles, TRUE, INFINITE);
 
-    // Vérifier le résultat de l'attente
+    // Verifier le resultat de l'attente
     if (waitResult == WAIT_FAILED) {
         printf("WaitForMultipleObjects failed (%d).\n", GetLastError());
         // Fermer les handles avant de retourner
@@ -109,7 +137,7 @@ int main() {
     // Fermer les handles des structures PROCESS_INFORMATION pour le premier processus
     CloseHandle(processInfo1.hProcess);
     CloseHandle(processInfo1.hThread);
-    // Fermer les handles des structures PROCESS_INFORMATION pour le deuxième processus
+    // Fermer les handles des structures PROCESS_INFORMATION pour le deuxieme processus
     CloseHandle(processInfo2.hProcess);
     CloseHandle(processInfo2.hThread);
 
@@ -127,7 +155,7 @@ int main() {
     hThreadImpair = CreateThread(NULL, 0, impairThread, NULL, 0, NULL);
 
     if (hThreadPair == NULL || hThreadImpair == NULL) {
-        printf("Erreur lors de la création du thread.\n");
+        printf("Erreur lors de la creation du thread.\n");
         return 1;
     }
 
@@ -138,10 +166,10 @@ int main() {
     DWORD dwWaitResult = WaitForMultipleObjects(2, hThreads, TRUE, INFINITE);
 
     switch (dwWaitResult) {
-        // Les threads ont terminé avec succès
+        // Les threads ont termine avec succes
     case WAIT_OBJECT_0:
     case WAIT_OBJECT_0 + 1:
-        printf("Les threads ont terminé avec succès.\n");
+        printf("Les threads ont termine avec succes.\n");
         break;
 
         // En cas d'erreur
